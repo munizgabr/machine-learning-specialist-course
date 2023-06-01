a63f920a0364a485f199469b74cf8ef1b792d236
munizgabr <gabrielrrtolkien@gmail.com>
2023-06-01 17:32:39 -0300

    download images

--- download-image.py
+++ download-image.py
@@ -44,7 +44,7 @@ def download_images(search_query, num_images):
     print('Download das imagens concluído.')
 
 # Exemplo de uso
-search_query = 'dog'
-num_images = 200
+search_query = 'cat'
+num_images = 40
 
 download_images(search_query, num_images)
